829b8523477791b32e203aa5c251e117b1fedfec
Stefanos Dovas <stefanosdcm@gmail.com>
2018-04-13 00:48:38 +0200

    add create form for recipe and flash messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,26 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = User.first
+    if @recipe.save
+      flash[:success] = "Your recipe was successfully created"
+      redirect recipe_path(@recipe)
+    else
+      render "new"
+    end
+  end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'pages/home', to: 'pages#home'
 
-  get "recipes", to: "recipes#index"
-  get "recipes/:id", to: "recipes#show", as: "recipe"
+  resources :recipes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
