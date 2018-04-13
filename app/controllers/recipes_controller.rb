caf269d57ff0cd578ead39aaa219606c69e8be43
Stefanos Dovas <stefanosdcm@gmail.com>
2018-04-13 10:52:26 +0200

    finish recipes controller and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,11 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -15,11 +16,29 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(recipe_params)
     @recipe.user = User.first
     if @recipe.save
-      flash[:success] = "Your recipe was successfully created"
+      flash[:success] = "Your recipe was created successfully"
       redirect recipe_path(@recipe)
     else
       render "new"
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted successfully"
+    redirect_to recipes_path
   end
 
 
@@ -27,5 +46,9 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:name, :description)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
