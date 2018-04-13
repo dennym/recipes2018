5aa6837b5afc65106b71cb9d113389d1a77e0064
Stefanos Dovas <stefanosdcm@gmail.com>
2018-04-13 11:02:44 +0200

    add bcrypt for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412201821) do
+ActiveRecord::Schema.define(version: 20180413085837) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180412201821) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
