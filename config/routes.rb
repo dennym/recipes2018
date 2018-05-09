65a0f5294ee4739e306bb2c4fc1e012177aa48ee
Denny Mueller <denny@dennymueller.de>
2018-05-09 12:48:11 +0200

    use correct quotation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  root 'pages#home'
   get 'pages/home', to: 'pages#home'
 
   resources :recipes do
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   resources :users, except: [:new]
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :ingredients, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
